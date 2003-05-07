1fa7afd168d1cd4acf2878c9134261f8edb9dc3f
milligan <milligan>
2003-05-07 06:55:49 +0000

    fixed for installing user/ heirarchy

--- website/tools/install-test.sh
+++ website/tools/install-test.sh
@@ -1,15 +1,23 @@
 #!/bin/sh
 
 DOCROOT=/var/www
-TEST=testing/
+TEST=testing
 
+pushd htdocs/
 cp *handler *css *mas *mhtml $DOCROOT/$TEST
 echo Installed *handler *css *mas *mhtml to $DOCROOT/$TEST
+
+pushd user/
+cp *handler *css *mas *mhtml $DOCROOT/$TEST/user
+echo Installed *handler *css *mas *mhtml to $DOCROOT/$TEST/user
+
+popd
+popd
 
 pushd $DOCROOT/$TEST
 
 perl -i -p -e "\$loc = '$TEST';" -e '
-   s{(<%method base>/)}{$1$loc};
+   s{(<%method base>/)}{$1$loc/};
    ' autohandler
 
 echo Configured autohandler
