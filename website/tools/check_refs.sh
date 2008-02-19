7fee38f6f02f8212397db234a0a1f6d8a855f9ac
Michael Milligan <mmilligan@falcon.localdomain>
2008-02-19 17:19:53 -0600

    Improve check_refs.sh

--- website/tools/check_refs.sh
+++ website/tools/check_refs.sh
@@ -1,14 +1,32 @@
 #!/bin/bash
 
-for f in $(find -type f | xargs -l basename)
- do count=$(grep -r $f * | wc -l)
- echo -ne "`tput setf 2`$f`tput op`:\t"
- len=$(echo "$f:" | wc -c)
- if [ $len -le 16 ]
-   then echo -ne "\t"
- fi
- if [ $count == 0 ]
-   then count="`tput setf 4`$count`tput op`"
- fi
- echo " $count matches"
+function spaces {
+  local i
+  for ((i=0; $i<$1; i++))
+  do echo -n " "
+  done
+}
+
+green=$(tput setf 2)
+red=$(tput setf 4)
+orig=$(tput op)
+files=$(find -type f | xargs -l basename)
+
+maxlen=0
+for f in $files
+  do len=$((${#f} + 1))
+  if [ $len -gt $maxlen ]
+    then maxlen=$len
+  fi
+done
+
+for f in $files
+  do count=$(grep -r $f * | wc -l)
+  echo -ne "${green}$f${orig}:"
+  len=$((${#f} + 1))
+  spaces $(( 3 + $maxlen - $len ))
+  if [ $count == 0 ]
+    then count="${red}$count${orig}"
+  fi
+  echo " $count matches"
 done
